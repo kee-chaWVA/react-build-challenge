3d0e30de755a81698e6efa240fd7baed56f36fde
kee-chaWVA <kee@wisvis.com>
2026-04-23 19:36:59 -0500

    added Fuse.js for fuzzy search capability, added error message

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,20 +1,40 @@
 import { useState } from 'react';
 import type { Movie } from '../types/movie'
-import { movies as initialMovies } from '../data/movies';
+import { movies } from '../data/movies';
 import Button from '../components/Button';
 import List from '../components/List';
 import Card from '../components/Card';
+import Fuse, { type FuseResult } from 'fuse.js';
+
+const fuse = new Fuse(
+  movies, {
+    keys: ['title', 'actor', 'director'],
+    threshold: 0.35
+  }
+)
 
 export default function SearchPage() {
-   const movies = initialMovies;
-  // const [ movies, setMovies] = useState<Movie[]>(initialMovies)
   const [ searchWord, setSearchWord ] = useState<string>('');
   const [ committedSearch, setCommittedSearch ] = useState<string>('')
+  const [ error, setError ] = useState<string>('')
 
-  const searchHandler = () => {
-    if(!searchWord.trim()) return;
+
+  const handleSearch = () => {
+    if(!searchWord.trim()) {
+      setError('You must enter a movie title, actor name, or director name.' )
+      return;
+    }
     setCommittedSearch(searchWord)
+    setError('')
   }
+
+  const handleChange = (e) => {
+    setError('')
+    setSearchWord(e.target.value)
+  }
+
+  const suggestions = fuse.search(searchWord)
+  const movieFromFuse = suggestions.slice(0,5).map( (result) =>  result.item )
 
   const results = movies.filter((movie) => {
     return movie.title.toLowerCase().includes(committedSearch.toLowerCase().trim())
@@ -22,12 +42,39 @@ export default function SearchPage() {
 
   return(
     <div>
-      <input
-        type='text'
-        value={searchWord}
-        onChange={(e) => setSearchWord(e.target.value)}
-      />
-      <Button onClick={searchHandler}>Search Movie</Button>
+      <div>
+        {error && <p>{error}</p>}
+      </div>
+      <div>
+        <input
+          type='text'
+          value={searchWord}
+          onChange={handleChange}
+        />
+        <Button onClick={handleSearch}>Search Movie</Button>
+      </div>
+      {searchWord.trim() && (
+        suggestions.length > 0 ? (
+          <div>
+            <List<Movie>
+              items={movieFromFuse}
+              renderItem={(movie) =>{
+                return (
+                  <li key={movie.id}>
+                    <p>{movie.title}</p>
+                  </li>
+                )}
+              }
+            />
+          </div>
+        ) : (
+          <div>
+            <p>
+              No matching movies
+            </p>
+          </div>
+        )
+      )}
       { committedSearch && (
         results.length > 0 ? (
           <List<Movie> 
