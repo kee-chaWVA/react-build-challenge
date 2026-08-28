6ed41394d272367a2089e4d4e48d73066e7bbd15
kee-chaWVA <kee@wisvis.com>
2026-08-28 12:01:36 -0500

    remove extra space

--- src/shared/utils/stringUtils.ts
+++ src/shared/utils/stringUtils.ts
@@ -1,4 +1,4 @@
-export   const capitalize = (value: string) => {
+export const capitalize = (value: string) => {
   if (!value) return "";
   return `${value.charAt(0).toUpperCase()}${value.slice(1)}`
 }
